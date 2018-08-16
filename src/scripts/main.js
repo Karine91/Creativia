732e0d8329ff3b210cab8f9f39c7039b5e1926dc
karine <kitana.91@mail.ru>
2018-08-16 22:25:21 +0300

    slider complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,6 @@
 import $ from "jquery";
+import Slider from "./modules/Slider";
 import Portfolio from "./modules/Portfolio";
 
+new Slider();
 new Portfolio();
---
+++ src/scripts/modules/Slider.js
@@ -0,0 +1,60 @@
+import $ from "jquery";
+
+class Slider {
+  constructor() {
+    this.$slider = $(".slider");
+    this.$slides = $(".slider__content");
+    this.slideActiveSelector = "slider__content--active";
+    this.$navLeft = $(".slider__nav-item--left");
+    this.$navRight = $(".slider__nav-item--right");
+    this.currentSlide = 0;
+    this.totalSlides = this.$slides.length;
+    this.animationCloseTime = 800;
+    this.events();
+    this.autoPlay();
+  }
+
+  events() {
+    this.$navLeft.click(this.slidePrev.bind(this));
+    this.$navRight.click(this.slideNext.bind(this));
+  }
+
+  slideNext(e) {
+    e.preventDefault();
+    clearInterval(this.timerId);
+    this.setActive(this.currentSlide + 1);
+    this.autoPlay();
+  }
+
+  slidePrev(e) {
+    e.preventDefault();
+    clearInterval(this.timerId);
+    this.setActive(this.currentSlide - 1);
+    this.autoPlay();
+  }
+
+  setActive(slideActive) {
+    this.$slider.removeClass(
+      "slider__animation slider__animation--close slider__animation--open"
+    );
+    this.$slider.addClass("slider__animation slider__animation--close");
+    setTimeout(() => {
+      this.currentSlide = (slideActive + this.totalSlides) % this.totalSlides;
+      let nextSlide = this.$slides.eq(this.currentSlide);
+      nextSlide
+        .show()
+        .delay(2000)
+        .addClass(this.slideActiveSelector);
+      this.$slides.not(nextSlide).hide();
+      this.$slider.addClass("slider__animation slider__animation--open");
+    }, this.animationCloseTime);
+  }
+
+  autoPlay() {
+    this.timerId = setInterval(() => {
+      this.setActive(this.currentSlide + 1);
+    }, 5000);
+  }
+}
+
+export default Slider;
